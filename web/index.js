d779035d5aaeda2be75f2cb5d004dbb3a98e5ce9
Tucker Connelly <web@tuckerconnelly.com>
2016-11-13 12:15:41 -0500

    Add support for web

--- web/index.js
+++ web/index.js
@@ -1,1 +1,4 @@
-export default {}
+module.exports = {
+  registerComponents: () => {},
+  nativeModules: {},
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'babel-polyfill',
+    './index.web',
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/assets',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    extensions: ['', '.js'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    }),
+  ],
+}
