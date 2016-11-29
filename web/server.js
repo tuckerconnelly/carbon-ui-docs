18f278321a049a8a4692c64c54d56a6b67035dba
Tucker Connelly <web@tuckerconnelly.com>
2016-11-29 13:09:38 -0500

    Fix serving aasa

--- web/server.js
+++ web/server.js
@@ -7,6 +7,10 @@ var path = require('path')
 var app = express()
 
 app.use('/assets', express.static(path.resolve(__dirname, 'assets')))
+app.get('/apple-app-site-association', function (req, res) {
+  res.set('Content-Type', 'application/json')
+  res.sendFile(path.resolve(__dirname, 'assets/apple-app-site-association'))
+})
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 })
