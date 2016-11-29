faeb4c6a116365837d7a3376eb7180963b2c6bd1
Tucker Connelly <web@tuckerconnelly.com>
2016-11-29 11:36:33 -0500

    Add code push

--- android/app/src/main/java/com/carbon_ui/android/MainApplication.java
+++ android/app/src/main/java/com/carbon_ui/android/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.walmartreact.ReactOrientationListener.ReactOrientationListener;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -16,6 +17,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+    @Override
+    protected String getJSBundleFile() {
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -25,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new ReactOrientationListener()
       );
     }
--- index.android.js
+++ index.android.js
@@ -1,8 +1,9 @@
 import { AppRegistry } from 'react-native'
+import codePush from 'react-native-code-push'
 import matchMedia from 'react-native-match-media'
 
 import App from './src/index'
 
 global.matchMedia = matchMedia
 
-AppRegistry.registerComponent('CarbonUIDocs', () => App)
+AppRegistry.registerComponent('CarbonUIDocs', () => codePush(App))
