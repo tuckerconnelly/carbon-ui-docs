91aec648df5e5581b89e9f6a8b9a763f5735f936
Tucker Connelly <web@tuckerconnelly.com>
2016-12-01 12:02:09 -0500

    Beef up related libraries

--- src/modules/Index/RelatedLibraries/index.js
+++ src/modules/Index/RelatedLibraries/index.js
@@ -15,8 +15,8 @@ class RelatedLibraries extends Component {
       <Content style={styles.base}>
         <Display1 style={styles.display1}>Related libraries</Display1>
         <Body1 style={styles.break}>
-          Carbon UI was built alongside a few libraries that pair really well
-          with it, especially in a universal context:
+          Carbon UI was built alongside a few libraries that pair well with it,
+          especially in a cross-platform context:
         </Body1>
         <View style={styles.smallBreak}>
           <Link
@@ -40,6 +40,29 @@ class RelatedLibraries extends Component {
             React apps. It supports back and forward buttons on Android and Web,
             deep linking, and server-side rendering. It also lets you compose
             routes declaratively.
+          </Body1>
+        </View>
+        <View style={styles.smallBreak}>
+          <Link
+            to="https://github.com/tuckerconnelly/react-native-match-media"
+            style={styles.dependencyLink}>
+            react-native-match-media
+          </Link>
+          <Body1>
+            This sweet little library polyfills matchMedia in react-native.
+            Useful for building tablet- and desktop-optimized apps in react-native.
+          </Body1>
+        </View>
+        <View style={styles.smallBreak}>
+          <Link
+            to="https://github.com/tuckerconnelly/react-native-ps"
+            style={styles.dependencyLink}>
+            react-native-ps
+          </Link>
+          <Body1>
+            react-native-ps lets you add platform-dependent styles without
+            spreading Platform.Select everywhere. Useful for quick
+            cross-platform styling.
           </Body1>
         </View>
         <View style={styles.smallBreak}>
