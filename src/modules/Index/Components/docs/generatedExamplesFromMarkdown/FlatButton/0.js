9484c6238e4ec780522ae029db68aa15a6f1c938
Tucker Connelly <web@tuckerconnelly.com>
2017-02-14 08:20:22 -0500

    Add disabled example for FlatButton

--- src/modules/Index/Components/docs/generatedExamplesFromMarkdown/FlatButton/0.js
+++ src/modules/Index/Components/docs/generatedExamplesFromMarkdown/FlatButton/0.js
@@ -5,4 +5,5 @@
  export default () =>
    <View style={{ justifyContent: 'flex-start', flexDirection: 'row' }}>
      <FlatButton>Hey I'm a button</FlatButton>
+     <FlatButton disabled>Hey I'm disabled</FlatButton>
    </View>
